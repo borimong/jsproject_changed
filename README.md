36c946fe3d748a686716617c51c6607c245c0507
borimong <kimhyunsu11@naver.com>
2022-01-24 16:53:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my first js project
+===========================
+# I like coding
---
+++ jsprj2.js
@@ -0,0 +1,11 @@
+let arr = ["I love you","I hate you","I don't have any idea with you"];
+
+console.log(arr[Math.floor(Math.random()*3)]);
+
+
+
+/*console.log("I love you");
+console.log("I hate you");
+console.log("I don't have any idea with you");
+console.log(arr[0]);*/
+//I love you, I hate you, I don't have any idea with you
